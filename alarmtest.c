314f2fff14d03ccee5aabb8519f7fb4b07dc6e0a
YINTAO2 <yintao2@kingsoft.com>
2017-03-29 16:06:51 +0800

    3)Assuming your code doesn't check that tf->esp is valid, implement a security attack on the kernel that exploits your alarm handler calling code.

--- alarmtest.c
+++ alarmtest.c
@@ -9,11 +9,21 @@ main(int argc, char *argv[])
 {
   int i;
   printf(1, "alarmtest starting\n");
-  alarm(10, periodic);
+  alarm(1, periodic);
+  //Challenges: 3)Assuming your code doesn't check that tf->esp is valid, implement a security attack on the kernel that exploits your alarm handler calling code.
+  //asm("movl %0,%%esp\n"
+//		  :/**/
+//		  :"r"(0x8dfbefb0));
   for(i = 0; i < 50*5000000; i++){
-    if((i++ % 500000) == 0)
-      write(2, ".", 1);
+    //if((i++ % 500000) == 0)
+      //write(2, ".", 1);
+      asm("nop\n");
   }
+      asm("movl $0x11,%eax\n movl %eax,%cr4\n");
+  //for(i = 0; i < 50*5000000; i++){
+  //  if((i++ % 500000) == 0)
+  //    write(2, ".", 1);
+  //}
   exit();
 }
 
